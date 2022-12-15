8d2332656c5ae160477efbd0c68c570f45890438
OlorinGray007 <luim@alumni.upenn.edu>
2022-12-15 11:04:18 -0800

    updated repository files

--- src/main/java/com/revature/repository/PlanetDao.java
+++ src/main/java/com/revature/repository/PlanetDao.java
@@ -1,14 +1,36 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.revature.models.Planet;
+import com.revature.utilities.ConnectionUtil;
 
 public class PlanetDao {
     
-    public List<Planet> getAllPlanets() {
-		// TODO Auto-generated method stub
-		return null;
+    public List<Planet> getAllPlanets() throws SQLException {
+		try (Connection con = ConnectionUtil.createConnection()){
+			String sql = "select * from planets";
+			Statement statement = con.createStatement();
+			ResultSet rs = statement.executeQuery(sql);
+
+			List<Planet> planets = new ArrayList();
+
+			while (rs.next()) {
+				Planet planet = new Planet();
+				planet.setId(rs.getInt(1));
+				planet.setName((rs.getString(2)));
+				planet.setOwnerId(rs.getInt(3));
+
+				planets.add(planet);
+			}
+			return planets;
+		}
 	}
 
 	public Planet getPlanetByName(String owner, String planetName) {
@@ -27,6 +49,26 @@ public class PlanetDao {
 	}
 
 	public void deletePlanetById(int planetId) {
-		// TODO Auto-generated method stub
+		try (Connection con = ConnectionUtil.createConnection()){
+			String sql = "delete from planets where id = ?";
+			PreparedStatement ps = con.prepareStatement(sql);
+			ps.setInt(1, planetId);
+			int rowsAffected = ps.executeUpdate();
+			System.out.println("Rows affected " + rowsAffected);
+
+		}catch (SQLException e){
+			System.out.println(e.getMessage());
+		} 
+	}
+
+	public static void main(String[] args) {
+		PlanetDao planetDao = new PlanetDao();
+		try {
+			System.out.println(planetDao.getAllPlanets());
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
+
+		//planetDao.deletePlanetById(2);
 	}
 }
--- src/main/java/com/revature/repository/UserDao.java
+++ src/main/java/com/revature/repository/UserDao.java
@@ -1,7 +1,14 @@
 package com.revature.repository;
 
+import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import com.revature.models.User;
 import com.revature.models.UsernamePasswordAuthentication;
+import com.revature.utilities.ConnectionUtil;
 
 public class UserDao {
     
@@ -10,7 +17,40 @@ public class UserDao {
     }
 
     public User createUser(UsernamePasswordAuthentication registerRequest){
-        return null;
+        try(Connection con = ConnectionUtil.createConnection()) {
+
+            String sql = "insert into users values (default, ?, ?)";
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, registerRequest.getUsername());
+            ps.setString(2, registerRequest.getPassword());
+
+            ps.execute();
+
+            ResultSet rs = ps.getGeneratedKeys();
+
+            User newUser = new User();
+            rs.next();
+
+            int newId = rs.getInt("id");
+            newUser.setId(newId);
+            newUser.setUsername(registerRequest.getUsername());
+            newUser.setPassword(registerRequest.getPassword());
+            return newUser;
+
+        }catch(SQLException e) {
+            System.out.println(e.getMessage());
+            return new User();
+        }
+    }
+
+    public static void main(String[] args) {
+        UserDao dao = new UserDao();
+
+        UsernamePasswordAuthentication newUser = new UsernamePasswordAuthentication();
+        newUser.setUsername("lombok");
+        newUser.setPassword("pass0word");
+        System.out.println(dao.createUser(newUser).getId());
     }
 
 }
