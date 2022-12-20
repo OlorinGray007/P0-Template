3e09e79ea6e1e0e98356b13a76aeda9ec5d28584
OlorinGray007 <luim@alumni.upenn.edu>
2022-12-20 12:50:47 -0800

    finished unimplemented methods

--- src/main/java/com/revature/controller/MoonController.java
+++ src/main/java/com/revature/controller/MoonController.java
@@ -1,5 +1,6 @@
 package com.revature.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.models.Moon;
@@ -15,37 +16,53 @@ public class MoonController {
 
 	public void getAllMoons(Context ctx) {
 		
+		try {
 		ctx.json(mService.getAllMoons()).status(200);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void getMoonByName(Context ctx) {
 		
+		try {
 		User u = ctx.sessionAttribute("user");
 		String moonName = ctx.pathParam("name");
 		
 		Moon m = mService.getMoonByName(u.getUsername(), moonName);
 		
 		ctx.json(m).status(200);
+		}catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void getMoonById(Context ctx) {
 		
+		try {
 		User u = ctx.sessionAttribute("user");
 		int moonId = ctx.pathParamAsClass("id", Integer.class).get();
 		
 		Moon m = mService.getMoonById(u.getUsername(), moonId);
 		
 		ctx.json(m).status(200);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void createMoon(Context ctx) {
 		
+		try {
 		Moon m = ctx.bodyAsClass(Moon.class);
 		User u = ctx.sessionAttribute("user");
 		
 		Moon outGoingMoon = mService.createMoon(u.getUsername(),m);
 		
 		ctx.json(outGoingMoon).status(201);
+		}catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void deleteMoon(Context ctx) {
@@ -59,10 +76,14 @@ public class MoonController {
 	
 	public void getPlanetMoons(Context ctx) {
 		
+		try {
 		int planetId = ctx.pathParamAsClass("id", Integer.class).get();
 		
 		List<Moon> moonList = mService.getMoonsFromPlanet(planetId);
 		
 		ctx.json(moonList).status(200);
+		}catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 }
--- src/main/java/com/revature/controller/PlanetController.java
+++ src/main/java/com/revature/controller/PlanetController.java
@@ -1,5 +1,6 @@
 package com.revature.controller;
 
+import java.sql.SQLException;
 import java.util.List;
 
 import com.revature.models.Moon;
@@ -17,38 +18,54 @@ public class PlanetController {
 
 	public void getAllPlanets(Context ctx) {
 		
+		try {
 		ctx.json(pService.getAllPlanets()).status(200);
+		} catch(SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void getPlanetByName(Context ctx) {
 		
+		try {
 		User u = ctx.sessionAttribute("user");
 		String planetName = ctx.pathParam("name");
 		
 		Planet p = pService.getPlanetByName(u.getUsername(), planetName);
 		
 		ctx.json(p).status(200);
+		} catch (SQLException e){
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void getPlanetByID(Context ctx) {
 		
+		try {
 		User u = ctx.sessionAttribute("user");
 		int planetId = ctx.pathParamAsClass("id", Integer.class).get();
 		
 		Planet p = pService.getPlanetById(u.getUsername(), planetId);
 		
 		ctx.json(p).status(200);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 
 	public void createPlanet(Context ctx) {
 		
+		try {
 		Planet planetToBeCreated = ctx.bodyAsClass(Planet.class);
 		User u = ctx.sessionAttribute("user");
 		
 		Planet createdPlanet = pService.createPlanet(u.getUsername(),planetToBeCreated);
 		
 		ctx.json(createdPlanet).status(201);
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 	}
 
 	public void deletePlanet(Context ctx) {
