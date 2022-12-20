047e9a022175c33e37336a6c320901719ff225a3
OlorinGray007 <luim@alumni.upenn.edu>
2022-12-20 12:48:06 -0800

    added logger

--- src/main/java/com/revature/MainDriver.java
+++ src/main/java/com/revature/MainDriver.java
@@ -1,11 +1,14 @@
 package com.revature;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.revature.controller.RequestMapping;
 
 import io.javalin.Javalin;
 
 public class MainDriver {
+
+	public static Logger logger = LoggerFactory.getLogger(MainDriver.class);
 
 	public static void main(String[] args) {
 		Javalin app = Javalin.create(confg ->{
